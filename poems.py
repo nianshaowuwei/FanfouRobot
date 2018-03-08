8f45127da99d3b1517d09238d8cc2449b22a63ea
zxallen <34667621+zxallen@users.noreply.github.com>
2018-03-08 09:49:22 +0800

    Update poems.py

--- poems.py
+++ poems.py
@@ -13,5 +13,7 @@ if __name__ == '__main__':
       if now.hour == 8 and now.minute < 1:
         send()
       elif now.hour == 4 and now.minute < 1:
-        
+        update()
+      elif now.hour %15 == 0:
+        check()
         
