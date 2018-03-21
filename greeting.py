afc6e3a4b9ff46a0b5789b63e733bf8052500ff9
zxallen <hellojiazhixiang@gmail.com>
2018-03-21 09:24:06 +0800

    first commit

--- greeting.py
+++ greeting.py
@@ -6,4 +6,6 @@ import fanfou
 
 # 请修改为你的 Consumer
 consumer = {'key': 'consumer key', 'secret': 'consumer secret'}
-
+# 请修改为你的 ID 和密码
+client = fanfou.XAuth(consumer, 'username', 'password')
+fanfou.bound(client)
