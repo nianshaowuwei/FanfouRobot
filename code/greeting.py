a41129085b719d17c0ceb6d2fe17436b981d9ba8
zxallen <hellojiazhixiang@gmail.com>
2018-03-23 16:57:12 +0800

    modify poem.py

--- code/greeting.py
+++ code/greeting.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 import time
 import random
 import shelve
@@ -9,3 +11,44 @@ consumer = {'key': 'consumer key', 'secret': 'consumer secret'}
 # 请修改为你的 ID 和密码
 client = fanfou.XAuth(consumer, 'username', 'password')
 fanfou.bound(client)
+
+
+db = shelve.open('greet.dbm')
+kw_words = ['晚安', '好梦', 'Good night', 'Good Night', 'good night', 'お休みなさい', 'おやすみなさい']
+reply_words = ['Good night~', 'Sweet dream~', 'おやすみなさい～', '晚安～', '晚安哦～']
+
+
+def work():
+    resp = client.statuses.home_timeline()
+    for item in resp.json():
+        status_id = item['id']
+        if status_id in db.keys():
+            continue
+
+        for kw in kw_words:
+            if kw in item['text']:
+                db[status_id] = True
+                send(item)
+                break
+
+
+def send(item):
+    reply = random.choice(reply_words)
+    status = '%s 转@%s %s' % (reply, item['user']['name'], item['text'])
+    body = {'status': status, 'repost_status_id': item['id']}
+
+    try:
+        client.statuses.update(body)
+    except:
+        pass
+
+
+if __name__ == '__main__':
+    while True:
+        now = datetime.datetime.now()
+        if now.hour in [22, 23, 0]:
+            try:
+                work()
+            except:
+                pass
+        time.sleep(60)
---
+++ the-end.rst
@@ -0,0 +1,36 @@
+是结束更是开始
+===============
+
+我们的教程至此就要告一段落了，由于篇幅短小和时间仓促（最重要是由于我水平较差），教程中必有不少错漏，若你发现了错误请告知我。
+
+希望我没有误导你，而是带你看了小小的新世界。如果你想多些了解 Python，下面是一些书的推荐：
+
+  - `《Python 简明教程》 <https://bop.molun.net/>`_ （快速入门）
+  - `《Python 语言及其应用》 <http://www.ituring.com.cn/book/1560>`_ （入门）
+  - `《Python 基础教程（第 2 版）》 <http://www.ituring.com.cn/book/159>`_ （入门）
+  - `《Python 网络数据采集》 <http://www.ituring.com.cn/book/1709>`_ （爬虫）
+  - `《Python网络编程（第3版）》 <http://www.ituring.com.cn/book/1552>`_ （网络）
+  - `《Flask Web 开发》 <http://www.ituring.com.cn/book/1449>`_ （Web）
+  - `《Python 参考手册（第4版•修订版）》 <http://www.epubit.com.cn/book/details/4072>`_ （手册）
+  - `《Python Cookbook（第3版）》 <http://www.epubit.com.cn/book/details/1765>`_ （进阶）
+  - `《流畅的 Python》 <http://www.ituring.com.cn/book/1564>`_ （进阶）
+
+希望你能写出有趣的机器人，请一定要告诉我，我会很开心。如果还有其他我能帮助你的，也请用邮箱联系我"email:hellojiazhixiang@gmail.com"
+
+如果阅读过程你觉得愉快或对你有帮助，可以请我喝杯咖啡 ^_^
+
+.. image:: image/tenpay.png
+
+.. image:: image/alipay.png
+
+.. image:: image/tenpay2.jpg
+::
+
+   要是有所得罪请原谅。本是出自一番好意，只是想显点粗浅技艺，那才是我们的初衷。
+                —— 威廉·莎士比亚，《仲夏夜之梦》（A Midsummer Night’s Dream）
+				 
+   （摘自《JavaScript 语言精粹》前言）
+
+
+Don't be evil
+-----------------
